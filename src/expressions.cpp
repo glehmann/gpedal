d73f1d0a59d968e70275e2ffbf71ac0a6bbc4b7e
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2020-06-27 23:21:33 +0200

    use a running median to compute the min and max values of the expression pedal
    just using the min or max is way to sensible to a noisy value

--- src/expressions.cpp
+++ src/expressions.cpp
@@ -1,20 +1,25 @@
 #include "expressions.h"
 #include <ResponsiveAnalogRead.h>
 #include "pin.h"
+#include <RunningMedian.h>
 
 #define EXPRESSIONS_SIZE 3
+#define EXPRESSIONS_MEDIAN_SAMPLES 21
 
 ResponsiveAnalogRead _expressions[EXPRESSIONS_SIZE];
 int _exp_min[EXPRESSIONS_SIZE];
 int _exp_max[EXPRESSIONS_SIZE];
 int _exp_status[EXPRESSIONS_SIZE];
+// must use pointers here, because there is no implicit constructor
+RunningMedian* _exp_median[EXPRESSIONS_SIZE];
 
 int expressions_map(int exp, int raw_value) {
-    if(_exp_min[exp] > raw_value) {
-        _exp_min[exp] = raw_value;
+    int median = _exp_median[exp]->getMedian();
+    if(_exp_min[exp] > median) {
+        _exp_min[exp] = median;
     }
-    if(_exp_max[exp] < raw_value) {
-        _exp_max[exp] = raw_value;
+    if(_exp_max[exp] < median) {
+        _exp_max[exp] = median;
     }
     // map to a larger range, then constrain the output to the normal range, in
     // order to make sure to have access to the edge values
@@ -32,16 +37,23 @@ void expressions_setup() {
     pinMode(PIN_D_EXPRESSION_RIGHT, OUTPUT);
     digitalWrite(PIN_D_EXPRESSION_RIGHT, HIGH);
     for(int exp=0; exp<EXPRESSIONS_SIZE; exp++) {
-        _expressions[exp].update();
-        _exp_min[exp] = _expressions[exp].getValue();
-        _exp_max[exp] = _expressions[exp].getValue();
-        _exp_status[exp] = expressions_map(exp, _expressions[exp].getValue());
+        _exp_median[exp] = new RunningMedian(EXPRESSIONS_MEDIAN_SAMPLES);
+        for(int rep=0; rep<EXPRESSIONS_MEDIAN_SAMPLES; rep++) {
+            _expressions[exp].update();
+            _exp_median[exp]->add(_expressions[exp].getValue());
+            delay(1);
+        }
+        Serial.printf("median: %f\n", _exp_median[exp]->getMedian());
+        _exp_min[exp] = _exp_median[exp]->getMedian();
+        _exp_max[exp] = _exp_median[exp]->getMedian();
+        _exp_status[exp] = expressions_map(exp, _exp_median[exp]->getMedian());
     }
 }
 
 void expressions_loop() {
     for(int exp=0; exp<EXPRESSIONS_SIZE; exp++) {
         _expressions[exp].update();
+        _exp_median[exp]->add(_expressions[exp].getValue());
         if(_expressions[exp].hasChanged()) {
             int status = expressions_map(exp, _expressions[exp].getValue());
             if(status != _exp_status[exp] && (_exp_max[exp] - _exp_min[exp] > 100)) {
